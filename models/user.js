c15b7b663ba9d5fd385e29bcabe873e4c49b08bb
trent <trntannan1@gmail.com>
2024-10-24 19:25:43 +1300

    schema

--- models/user.js
+++ models/user.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+const UserCollection = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   profile: {
     fullName: { type: String },
+    numberOfKids: { type: String },
     school: { type: String },
-    kidCount: { type: String },
     bio: { type: String },
-    profilePic: {
-      data: Buffer,
-      contentType: String,
-    },
   },
+  profilePic: { type: mongoose.Schema.Types.ObjectId, ref: "ProfilePicture" },
+  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserCollection);
