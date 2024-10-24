2d8acb8a924b6cb2de53b8eec4af30dcfbcf82b2
trent <trntannan1@gmail.com>
2024-10-24 15:00:21 +1300

    changed from bcrypt to bcryptjs

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 connectToMongoDB()
   .then(() => {
+    console.log("Connected to MongoDB");
     app.use("/api/user", userRouter);
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
@@ -19,4 +19,5 @@ connectToMongoDB()
   })
   .catch((error) => {
     console.error("Failed to connect to MongoDB:", error);
+    process.exit(1);
   });
