b649cd0a8986a6238a8524d06286b680c997972a
trent <trntannan1@gmail.com>
2024-10-31 05:02:26 +1300

    annoying

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
