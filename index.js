dae648ebd06203256f40a7ca7210535114666cbf
trent <trntannan1@gmail.com>
2024-10-31 04:34:05 +1300

    last backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,7 +13,6 @@ connectToMongoDB()
   .then(() => {
     console.log("Connected to MongoDB");
     app.use("/api/user", userRouter);
-    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
