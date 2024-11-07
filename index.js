4a90a8192b6148f92bb59d9e4cec953543838867
trent <trntannan1@gmail.com>
2024-11-08 08:43:40 +1300

    proxy thingy

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.set("trust proxy", 1);
+app.get("/ip", (request, response) => response.send(request.ip));
+
 app.use(cors());
 app.use(express.json());
 
