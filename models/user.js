b18e4ddb88aaef0d577dbf67358b125ddaad80f9
trent <trntannan1@gmail.com>
2024-11-14 06:10:54 +1300

    more

--- models/user.js
+++ models/user.js
@@ -12,42 +12,40 @@ const userSchema = new mongoose.Schema(
         contentType: String,
       },
     },
-    groups: [
-      {
-        name: { type: String, required: true },
-        creator: {
+    groups: {
+      name: { type: String, required: true },
+      creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      members: [
+        {
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
           required: true,
         },
-        members: [
-          {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
+      ],
+      startTime: { type: Date, required: true },
+      routes: [
+        {
+          start: {
+            latitude: { type: String, required: true },
+            longitude: { type: String, required: true },
           },
-        ],
-        startTime: { type: Date, required: true },
-        routes: [
-          {
-            start: {
-              latitude: { type: String, required: true },
-              longitude: { type: String, required: true },
-            },
-            end: {
-              latitude: { type: String, required: true },
-              longitude: { type: String, required: true },
-            },
-            waypoints: [
-              {
-                latitude: String,
-                longitude: String,
-              },
-            ],
+          end: {
+            latitude: { type: String, required: true },
+            longitude: { type: String, required: true },
           },
-        ],
-      },
-    ],
+          waypoints: [
+            {
+              latitude: String,
+              longitude: String,
+            },
+          ],
+        },
+      ],
+    },
   },
   {
     timestamps: true,
